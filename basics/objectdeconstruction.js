e184c37cb609cbb248904236f1e8f8957354b6c3
nin bond <ianbond759@gmail.com>
2020-10-28 22:22:59 -0400

    addition to objectdeconstruction.js with default value for dict[key] = value

--- basics/objectdeconstruction.js
+++ basics/objectdeconstruction.js
@@ -8,3 +8,17 @@ const user = {
   }
   
   renderUser(user);
+
+  const blog = {
+    title: 'My great post',
+    summary: 'Summary of my post'
+  }
+  
+  const openGraphMetadata = ({ title, summary = 'A DailySmarty Post' }) => {
+    console.log(`
+      og-title=${title}
+      og-description=${summary}
+    `);
+  }
+  
+  openGraphMetadata(blog);
