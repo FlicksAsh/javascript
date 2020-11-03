37d32f800adb2dea765ec1dc20d60749bb800b14
Ninian Bond <ninianbond@Ninians-MacBook-Pro.local>
2020-11-03 14:33:31 -0500

    created lodashfiltertimes.html containing javascript run in the console of the browser

--- exercises/weightedlottery.js
+++ exercises/weightedlottery.js
@@ -1,15 +1,39 @@
+// const weightedLottery = weights => {
+//     // keep track of your  weights
+//     // ['green', 'yellow', 'yellow', 'red', 'red', 'red']
+//     let containerArray = [];
+
+//     Object.keys(weights).forEach(key =>{
+//         for (i = 0; i < weights[key]; i++) {
+//             containerArray.push(key);
+//         }
+//     })
+//     return containerArray[Math.floor(Math.random() * containerArray.length)]
+// }
+
+// const weights = {
+//     winning: 1,
+//     losing: 1000
+// }
+
+// console.log(weightedLottery(weights))
+
+
+
+
 const weightedLottery = weights => {
     // keep track of your  weights
     // ['green', 'yellow', 'yellow', 'red', 'red', 'red']
     let containerArray = [];
 
-    Object.keys(weights).forEach(key =>(
-        for (let i = 0; i < weights[key]; i++) {
+    
+    for (var key in weights) {
+        for (i = 0; i < weights[key]; i++) {
             containerArray.push(key);
         }
-    ))
+    }
 
-    return containerArray[(Math.random() * containerArray.length) | 0];
+    return containerArray[Math.floor(Math.random() * containerArray.length)]
 }
 
 const weights = {
@@ -17,4 +41,4 @@ const weights = {
     losing: 1000
 }
 
-weightedLottery(weights)
+console.log(weightedLottery(weights))
